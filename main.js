21d0a08e23c7e7654c408d199c23a513354888b7
Yeisson Jimenez <yeissonozy18@gmail.com>
2022-11-13 21:32:41 -0500

    Uniendo-MenuMobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobilrMenu = document.querySelector('.mobile-menu');
 
-
-menuEmail.addEventListener('click', toggleDesktopMenu);
+// agregando los eventos del click para que aparezca el menu
+menuEmail.addEventListener('click', toggleDesktopMenu); 
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobilrMenu.classList.toggle('inactive');
 }
